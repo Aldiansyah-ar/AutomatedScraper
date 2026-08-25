28d87f742e59eb4c0b6041273cb9171d3a80ee12
Aldiansyah Anugrah Ramadhan <141223746+Aldiansyah-ar@users.noreply.github.com>
2026-08-25 20:31:15 +0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import csv
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
